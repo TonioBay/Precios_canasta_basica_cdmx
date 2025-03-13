024ea7c43481a0fa28a51ed4737b6697aaf8d9d0
Antonio Sanchez <sanchezb.antonio10@gmail.com>
2025-03-13 15:53:00 -0600

    Iteración por página

--- main.py
+++ main.py
@@ -8,26 +8,29 @@ from funciones import fecha, df_list
 ruta_pdf = os.path.join('File', 'ano-2024.pdf')
 tables = tabula.read_pdf(
 ruta_pdf,
-pages = '1-5',
+pages = '3-8',
 multiple_tables = True,
 lattice= True,
 stream = True,
 guess = False,
 pandas_options= {"header": None}
 )
-## Extracción de fecha por pagina
-n_pagina= 3
-pdf = fitz.open(ruta_pdf)
-fecha = fecha(pdf,n_pagina)
-# Limpieza del dataframe
 
-df = tables[n_pagina].dropna(axis = 0, how = 'all').reset_index(drop = True)
-df = df.drop(range(0,2)).reset_index(drop = True)
-## Union de dfs por pagina
-dfs = df_list(df)      
-df_final = pd.concat(dfs, ignore_index= True)
-## Formato por columna
-df_final['Fecha'] = fecha #Columna con la fecha de la pagina
-df_final['Sucursal'] = df_final['Sucursal'].str.replace(r'[\r\n]', ' ', regex=True) # remplazar valores en columna 'Sucursal'
-df_final['Precio'] = df_final['Precio'].str.replace('$','').astype(float) # Eliminación del simbolo en la columna 'Precio' y transformación de string a float
-print(df_final)
+for n_pagina in  range(len(tables)):
+    ## Extracción de fecha por pagina
+    n_pagina= 0
+    pdf = fitz.open(ruta_pdf)
+    fecha1 = fecha(pdf,n_pagina)
+    # Limpieza del dataframe
+
+    df = tables[n_pagina].dropna(axis = 0, how = 'all').reset_index(drop = True)
+    df = df.drop(range(0,2)).reset_index(drop = True)
+    ## Union de dfs por pagina
+    dfs = df_list(df)      
+    df_final = pd.concat(dfs, ignore_index= True)
+    ## Formato por columna
+    df_final['Fecha'] = fecha1 #Columna con la fecha de la pagina
+    df_final['Sucursal'] = df_final['Sucursal'].str.replace(r'[\r\n]', ' ', regex=True) # remplazar valores en columna 'Sucursal'
+    df_final['Precio'] = df_final['Precio'].str.replace('$','').astype(float) # Eliminación del simbolo en la columna 'Precio' y transformación de string a float
+    
+    print(df_final)
