3f0824176f77805b111d9d5d1bb00cd590b10ec2
Antonio Sanchez <sanchezb.antonio10@gmail.com>
2025-03-13 21:55:57 -0600

    Lista con paginas con error

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import pandas as pd
 import fitz
 from funciones import fecha, df_list
 from sqlalchemy import create_engine
+import numpy as np
 
 #Extracción de tablas
 ruta_pdf = os.path.join('File', 'ano-2024.pdf')
 tables = tabula.read_pdf(
 ruta_pdf,
-pages = 'all',
+pages = '1-678',
 multiple_tables = True,
 lattice= True,
 stream = True,
@@ -17,12 +18,17 @@ guess = False,
 pandas_options= {"header": None}
 )
 df_final = pd.DataFrame()
+fecha_ant = ''
+paginas_error =[]
 for n_pagina in  range(len(tables)):
     ## Extracción de fecha por pagina
-   
+    
     pdf = fitz.open(ruta_pdf)
-    fecha1 = fecha(pdf,n_pagina)
-    # Limpieza del dataframe
+    try:
+        fecha1 = fecha(pdf,n_pagina)
+    except:
+        fecha1 = np.nan
+        paginas_error.append(n_pagina +1)
 
     df = tables[n_pagina].dropna(axis = 0, how = 'all').reset_index(drop = True)
     df = df.drop(range(0,2)).reset_index(drop = True)
@@ -32,7 +38,7 @@ for n_pagina in  range(len(tables)):
     ## Formato por columna
     df_page['Fecha'] = fecha1 #Columna con la fecha de la pagina
     df_page['Sucursal'] = df_page['Sucursal'].str.replace(r'[\r\n]', ' ', regex=True) # remplazar valores en columna 'Sucursal'
-    df_page['Precio'] = df_page['Precio'].str.replace('$','').astype(float) # Eliminación del simbolo en la columna 'Precio' y transformación de string a float
+    df_page['Precio'] = df_page['Precio'].str.replace('$','') # Eliminación del simbolo en la columna 'Precio' y transformación de string a float
     df_page = df_page[['Fecha', 'Canal de abasto', 'Sucursal', 'Tipo de costo', 'Producto','Precio']]
     df_final = pd.concat([df_final,df_page], ignore_index= True)
     print(f'Página {n_pagina + 1} procesada')
@@ -41,4 +47,5 @@ engine = create_engine("sqlite:///canasta_basica.db")
 df_final.to_sql("canasta_basica", engine, if_exists= "append", index = False)
 engine.dispose()
 df_final.to_csv('canasta_basica.csv', index = False , encoding='latin1')
+pd.DataFrame({'errores': paginas_error}).to_csv('errores.csv', index = False)
 print('Carga terminada')
